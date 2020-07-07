366a1a341f281fc08a6384c68a38cda1411bdba4
Subramania Prasad <suprasad@freshworks.com>
2020-07-07 12:37:41 +0530

    fixing fami to family_name

--- encoding_decoding.py
+++ encoding_decoding.py
@@ -2,7 +2,7 @@ import jwt
 from datetime import datetime
 def jwt_token(nonce):
     private_key = open('jwtRS256.key').read()
-    payload = {'sub': 'ue839','email':'jwttest@freshworks.com','iat': datetime.utcnow(),'nonce': nonce,'fami': 'Test','given_name':'JWT', 'company':'testcompany123'}
+    payload = {'sub': 'ue839','email':'jwttest@freshworks.com','iat': datetime.utcnow(),'nonce': nonce,'family_name': 'Test','given_name':'JWT', 'company':'testcompany123'}
     headers = { "alg": "RS256", "typ": "JWT"}
     token = jwt.encode(payload,private_key,algorithm='RS256',headers=headers).decode('utf=-8')
     #print(token)
